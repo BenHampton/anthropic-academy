d861eb755028041587d127c162bab18552f1873e
Ben Hampton <BenHampton11@yahoo.com>
2026-04-25 15:43:38 -0500

    Useful hooks

--- Introduction to Model Context Protocol/Hooks and the SDK/queries/hooks/read_hook.js
+++ Introduction to Model Context Protocol/Hooks and the SDK/queries/hooks/read_hook.js
@@ -9,7 +9,6 @@ async function main() {
   const readPath =
     toolArgs.tool_input?.file_path || toolArgs.tool_input?.path || "";
 
-    console.log('TEST')
   // TODO: ensure Claude isn't trying to read the .env file
   if (readPath.includes('.env')) {
     console.error("You can not read the .env file");
